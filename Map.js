8230ff4ab8f555e2fa347fe3f3e0f30888c6944f
asmarana <asmaiftikhar777@gmail.com>
2022-09-01 11:35:29 +0500

    Map dynamic polygon and URLtile

--- Map.js
+++ Map.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { View, TouchableOpacity, StyleSheet,Dimensions  } from 'react-native'
-import MapView ,{Marker} from 'react-native-maps'
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native'
+import MapView, { Marker, Polygon } from 'react-native-maps'
 import MapConfig from './Mapconfig'
 import * as Location from 'expo-location';
 const ApiKey = 'AIzaSyCY1oDgXTf55jiJBGLsiTsCgf9DyrlU66E';
@@ -17,21 +17,26 @@ export default class Map extends Component {
             marginBottom: 1,
             locationChosen: false,
             CurrentLocation: '',
-            city: ''
+            city: '',
+            markers: []
       }
-      updatedLocation =region => {
+      onMapPress(e) {
+            alert("coordinates:" + JSON.stringify(e.nativeEvent.coordinate));
+
+      }
+      updatedLocation = region => {
             this.setState({
                   region
             })
-            this.getAddress(this.state.region.latitude,this.state.region.longitude);
+            this.getAddress(this.state.region.latitude, this.state.region.longitude);
       }
 
       getAddress = async (lat, lng) => {
             Location.setGoogleApiKey(ApiKey)
             try {
                   let res = await Location.reverseGeocodeAsync({ latitude: lat, longitude: lng }, { useGoogleMaps: true })
-                //   console.log(res);
-                  let Address = res[0].name.concat(" " + res[1].name).concat (","+res[0].region)
+                  //   console.log(res);
+                  let Address = res[0].name.concat(" " + res[1].name).concat("," + res[0].region)
                   let city = res[0].city
                   this.setState({
                         CurrentLocation: Address,
@@ -43,64 +48,80 @@ export default class Map extends Component {
 
       }
 
-      onChangeValue = CurrentLocation=> {
-console.log('Current',this.state.CurrentLocation);
+      onChangeValue = CurrentLocation => {
+            console.log('Current', this.state.CurrentLocation);
             this.setState({
                   CurrentLocation
             })
       }
+      handleMarkerPress(event) {
+            const markerID = event.nativeEvent.identifier;
+            alert(markerID);
+        }
 
       render() {
-        let marker =
-              <View style={{ top: '37%', bottom: '50%', marginLeft: 153, marginTop: 40, position: "absolute", zIndex: 100 }}>
-                    <Marker
-                  style={{zIndex:10000}}
-                          coordinate={this.state.region}
-                          title={this.state.CurrentLocation}
-                          description={this.state.city}
-                          ref={ref => this.mark = ref}
-                    
-                    >
-                    </Marker>
-              </View>
-        return (
-              <View style={{ flex: 1 }}>
+            let marker =
+                  <View style={{ top: '37%', bottom: '50%', marginLeft: 153, marginTop: 40, position: "absolute", zIndex: 100 }}>
+                        <Marker
+                              style={{ zIndex: 10000 }}
+                              coordinate={this.state.region}
+                              title={this.state.CurrentLocation}
+                              description={this.state.city}
+                              ref={ref => this.mark = ref}
 
-                    <MapView
-                          style={{ flex: 1, marginBottom: this.state.marginBottom }}
-                          showsMyLocationButton={true}
-                          showsUserLocation={true}
-                          customMapStyle={MapConfig}
-                          initialRegion={this.state.region}
-                          onRegionChange={this.onChangeValue}
-                          zoomControlEnabled={true}
-                          followsUserLocation={true}
-                          onRegionChangeComplete={this.updatedLocation}
-                          ref={map => {
-                                this.map = map;
-                          }}>
-                          {marker}
-                    </MapView>
-                    <View></View>
-              </View>
-        )
-  }
+                        >
+                        </Marker>
+                  </View>
+            return (
+                  <View style={{ flex: 1 }}>
+
+                        <MapView
+                              style={{ flex: 1, marginBottom: this.state.marginBottom }}
+                              showsMyLocationButton={true}
+                              showsUserLocation={true}
+                              customMapStyle={MapConfig}
+                              initialRegion={this.state.region}
+                              onRegionChange={this.onChangeValue}
+                              zoomControlEnabled={true}
+                              followsUserLocation={true}
+                              onRegionChangeComplete={this.updatedLocation}
+                              ref={map => {
+                                    this.map = map;
+                              }}
+                              onPress={this.onMapPress.bind(this)}>
+                              {marker}
+                              <Polygon fillColor="rgba(000, 000, 200, 0.3)"
+                                    strokeColor="rgba(000, 000, 200, 0.3)"
+                                    coordinates={[
+                                          { latitude: 35.7645, longitude: 73.7679 },
+                                          { latitude: 36.7420, longitude: 72.7175 },
+                                          { latitude: 37.7499, longitude: 72.7277 },
+                                          { latitude: 38.7599, longitude: 72.7477 },
+                                          { latitude: 35.7645, longitude: 73.7679 }
+
+                                    ]}>
+                              </Polygon>
+                        </MapView>
+                        <Text style={{ position: 'absolute', bottom: 0, right: 0, zIndex: 1 }}>Text</Text>
+                  </View>
+            )
+      }
 }
 
 const styles = StyleSheet.create({
 
-  btntext: {
-        fontSize: "2%",
-        color: '#241416',
-  },
-  button:
-  {
-        height: "6.5%",
-        width: "70%",
-        backgroundColor: '#0000',
-        justifyContent: "center",
-        alignItems: 'center',
-        borderRadius: 0,
+      btntext: {
+            fontSize: "2%",
+            color: '#241416',
+      },
+      button:
+      {
+            height: "6.5%",
+            width: "70%",
+            backgroundColor: '#0000',
+            justifyContent: "center",
+            alignItems: 'center',
+            borderRadius: 0,
 
-  },
+      },
 })
